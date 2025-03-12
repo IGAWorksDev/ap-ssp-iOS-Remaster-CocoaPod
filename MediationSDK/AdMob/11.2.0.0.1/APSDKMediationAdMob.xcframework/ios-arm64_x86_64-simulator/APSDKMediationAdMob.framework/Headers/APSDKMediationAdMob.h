891e93321598cc2075ef6c986bf428ef879b795a
Odin-AP <odin.song@adpopcorn.com>
2025-03-12 18:10:45 +0900

    [fix] static 변경 테스트용 .1 ver

---
+++ MediationSDK/AdMob/11.2.0.0.1/APSDKMediationAdMob.xcframework/ios-arm64_x86_64-simulator/APSDKMediationAdMob.framework/Headers/APSDKMediationAdMob.h
@@ -0,0 +1,18 @@
+//
+//  APSDKMediationAdMob.h
+//  APSDKMediationAdMob
+//
+//  Created by Odin.송황호 on 12/9/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APSDKMediationAdMob.
+FOUNDATION_EXPORT double APSDKMediationAdMobVersionNumber;
+
+//! Project version string for APSDKMediationAdMob.
+FOUNDATION_EXPORT const unsigned char APSDKMediationAdMobVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APSDKMediationAdMob/PublicHeader.h>
+
+
---
+++ MediationSDK/Mintegral/7.6.9.0.1/APSDKMediationMintegral.xcframework/ios-arm64_x86_64-simulator/APSDKMediationMintegral.framework/Headers/APSDKMediationMintegral.h
@@ -0,0 +1,18 @@
+//
+//  APSDKMediationMintegral.h
+//  APSDKMediationMintegral
+//
+//  Created by Odin.송황호 on 12/9/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APSDKMediationMintegral.
+FOUNDATION_EXPORT double APSDKMediationMintegralVersionNumber;
+
+//! Project version string for APSDKMediationMintegral.
+FOUNDATION_EXPORT const unsigned char APSDKMediationMintegralVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APSDKMediationMintegral/PublicHeader.h>
+
+
